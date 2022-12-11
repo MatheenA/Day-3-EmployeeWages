dac198d159a26fbd2378674afd68c0e19b53a4e6
MatheenA <abdulmatheen1001@gmail.com>
2022-12-11 15:25:18 +0530

    add your commit

---
+++ Attendancecheck.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+public class Attendancecheck {
+
+    public static void main(String[] args) {
+        int isPresent=1;
+        Random r = new Random();
+        int attendance = r.nextInt(3);
+        if(attendance == isPresent){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is absent");
+        }
+    }
+}
---
+++ Dailywage.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+    class DailyEmpWage {
+        public static void main(String[] args) {
+            int isPresent = 1;
+            final int WAGE_PER_HOUR = 20;
+            final int WORKING_HRS_PER_DAY = 8;
+
+            Random r = new Random();
+            int attendance = r.nextInt(3);
+            if (attendance == isPresent) {
+                int dailywage = WAGE_PER_HOUR * WORKING_HRS_PER_DAY;
+                System.out.println(dailywage);
+            } else {
+                System.out.println("Employee is absent");
+            }
+
+        }
+    }
